3c1e5a519d02fc7a9f5298e22369feeeeabe0580
zinebMachrouh <zinebmac49@gmail.com>
2024-12-28 17:03:54 +0100

    Enhance dashboard and category components: improve responsiveness, update styles, and add statistics component

---
+++ src/app/modules/dashboard/components/statistics/statistics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics',
+  imports: [],
+  templateUrl: './statistics.component.html',
+  styleUrl: './statistics.component.scss'
+})
+export class StatisticsComponent {
+
+}
--- src/app/shared/components/dashboard/dashboard.component.ts
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
-import {NgOptimizedImage} from '@angular/common';
-import {TaskListComponent} from '../../../modules/tasks/components/task-list/task-list.component';
-import {StatsComponent} from '../../../modules/dashboard/components/stats/stats.component';
-import {CategoryListComponent} from '../../../modules/categories/components/category-list/category-list.component';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
+import { TaskListComponent } from '../../../modules/tasks/components/task-list/task-list.component';
+import { StatsComponent } from '../../../modules/dashboard/components/stats/stats.component';
+import { CategoryListComponent } from '../../../modules/categories/components/category-list/category-list.component';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [
     RouterLink,
     RouterLinkActive,
@@ -16,8 +17,17 @@ import {CategoryListComponent} from '../../../modules/categories/components/cate
     CategoryListComponent
   ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  @Input() isVisible: boolean = false;
+  @Output() close = new EventEmitter<void>();
+  openSidebar: boolean = false;
 
+  toggleSidebar(): void {
+    this.openSidebar = !this.openSidebar;
+    if (!this.openSidebar) {
+      this.close.emit();
+    }
+  }
 }
